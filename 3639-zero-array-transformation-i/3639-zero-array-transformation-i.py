1ccc07e66371c9795e5ca177193a14f9f31e41ba
Pratik Kumar Singh <pkumarjee2006@gmail.com>
2025-05-20 18:41:25 +0530

    Time: 694 ms (12.2%), Space: 48.3 MB (52.44%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isZeroArray(self, nums, queries):
+        memo = defaultdict(int)
+        curr = 0
+        for l, r in queries:
+            memo[l] += 1
+            memo[r + 1] -= 1
+        print(memo)
+        for idx, val in enumerate(nums):
+            curr += memo.get(idx, 0)
+            if curr < val:
+                return False
+        return True
